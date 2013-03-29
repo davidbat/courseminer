b06acba6f0bed4d1e38709bc638bf7cd8a07a53d
Rushabh <rushabh@ubuntu.(none)>
2013-03-29 12:58:41 -0400

    Added parser.v4.py which is the final parser

--- selenium/parser.v2.py
+++ selenium/parser.v2.py
@@ -2,21 +2,19 @@ import os,re
 
 path = "out/"
 
-
 for (path, dirs, files) in os.walk(path):
 	for file in files:
-		if "Summer_2_2012.file" in file:
-			logFid=open(file+".log",'w')
-			count = 0 
+		if ".file" in file:
+			logFid=open(file+".log.temp",'w')
 			print file
 			
 			for line in open(path+file).readlines():
 				line=line.lower()				
-				try:	
-					print line[line.index('=">')+3:].split('-')
-					logFid.write("\n"+str(line[line.index('=">')+3:].split('-'))+',')
+				try:
+				  	#print line[line.index('=">')+3:].split('-')
+				  	lst = line[line.index('=">')+3:].split('-')
+				  	logFid.write("\n"+str(lst[0])+','+str(lst[1])+','+str(lst[2])+','+str(lst[5][:lst[5].index('</a>')])+',')
 				except ValueError:
 					length = len(line.split('>'))
-					print line.split('>')[length/2]
+					#print line.split('>')[length/2]
 					logFid.write(line.split('>')[length/2]+',')
-
