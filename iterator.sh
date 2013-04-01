5e799af40895fef7f529c4915b348457abea4dbb
Dave <davidbatelu@gmail.com>
2013-04-01 19:38:24 -0400

    Iterator complete

--- iterator.sh
+++ iterator.sh
@@ -1,13 +1,25 @@
-if [ $# -eq 0 ]
+
+
+if [ $# -lt 1 ]
+then
+	echo Need to supply semester
+	exit
+fi
+cur_sem=$1
+if [ $# -eq 1 ]
 then
 	min_sup="0.3%"
 else
-	min_sup=$1
+	min_sup=$2
 fi
-
-cat grad_info.csv | grep -v "Spring 2013" | grep -v "Fall 2012"  > grad_info2.csv
+echo $cur_sem
+my_set=`python find_sems.py "$cur_sem"`
+echo $my_set
+cat grad_info.csv | egrep -v "$my_set"  > grad_info2.csv
 
 new_stud=`python eligible_student_sem.py "Fall 2012" GR | cut -d " " -f 2`
+
+echo $new_stud new students
 
 python pre_proc.py >/dev/null
 
