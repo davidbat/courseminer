29f5bce54b2f6e93eb58b7811f7027f7367a986b
Dave <davidbatelu@gmail.com>
2013-04-04 20:25:48 -0400

    java run modified

--- java_run.sh
+++ java_run.sh
@@ -1,1 +1,7 @@
-java -jar spmf.jar run FPGrowth_itemsets courses.txt output.txt 0.3%
+perc=0.3%
+if [ $# -eq 1 ]
+then
+  perc=$1
+fi
+echo $perc
+java -jar spmf.jar run FPGrowth_itemsets courses.txt output.txt $perc
