9113f34b2fa06e89b17d14a033aa36ec2df39f35
Dave <davidbatelu@gmail.com>
2013-04-15 23:28:18 -0400

    Sched prints removed

--- sched.py
+++ sched.py
@@ -17,7 +17,7 @@ def read_courses_prof_hash(prof_course_fn = "courses_professors.txt"):
 
 		prof_course_hash = {}
 		rank = {}
-		print prof_course_tmphash
+		#print prof_course_tmphash
 		for course in prof_course_tmphash:
 			prof_list = prof_course_tmphash[course]
 			tmp_list = map(lambda row:row[0], (sorted(prof_list, key=lambda row:row[1], reverse=True)))
@@ -59,7 +59,7 @@ def brute_helper(prof_course_hash, profs, courses, penalty, course_offset, prof_
 			prof_course_hash = available_prof_hasher(prof_course_hash, profs)
 		else:
 			# course can't be classified
-			print course
+			#print course
 			return float("inf")
 	return score
 
@@ -71,7 +71,7 @@ def brute_force(prof_course_hash, profs, courses, penalty = 100):
 	for i in range(len(courses)):
 		for j in range(len(prof_course_hash[courses[i]])):
 			score = brute_helper(copy.deepcopy(prof_course_hash), set(profs), courses, penalty, i, j)
-			print "here:", courses[i], j, score
+			#print "here:", courses[i], j, score
 			#if score != float("inf"):
 			#	print "Assigned:"
 			#	pprint(brute_reenact(copy.deepcopy(prof_course_hash), set(profs), courses, penalty, i, j))
@@ -92,9 +92,9 @@ def pprint(my_hash):
 
 
 available_profs = map(lambda prof:prof.strip(), open("available_profs.txt").readlines())
-print "available_profs" , available_profs
+#print "available_profs" , available_profs
 predicted_courses = map(lambda row:row.strip().split()[0], open("Predicted_values.txt").readlines())
-print "predicted_courses", predicted_courses
+#print "predicted_courses", predicted_courses
 all_cs_courses = map(lambda prof:prof.strip(), open("ccis_courses.txt").readlines())
 non_cs_courses = set(predicted_courses) - set(all_cs_courses)
 predicted_cs_courses = list(set(predicted_courses) - non_cs_courses)
@@ -102,11 +102,11 @@ course_info = read_courses_prof_hash()
 
 seen_cs_courses = []
 for course in predicted_cs_courses:
-	if course not in course_info:
-		print course, " is a new course. We can't schedule it."
-	else:
+	if course in course_info:
+		#print course, " is a new course. We can't schedule it."
+	#else:
 		seen_cs_courses.append(course)
-print "seen_cs_courses", seen_cs_courses
+#print "seen_cs_courses", seen_cs_courses
 available_profs_course_hash = available_prof_hasher(course_info, available_profs)
 #print "available_profs_course_hash", available_profs_course_hash
 assigned_courses = brute_force(available_profs_course_hash, available_profs, seen_cs_courses)
@@ -114,7 +114,7 @@ assigned_courses = brute_force(available_profs_course_hash, available_profs, see
 if assigned_courses == {}:
 	# we have to redo deleting a course?
 	# delete smallest course
-	print "Oops"
+	print "Not Assigned"
 else:
 	print "Assigned"
 	pprint(assigned_courses)
